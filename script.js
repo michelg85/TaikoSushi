95b7ba904c340fd7b1c876c3891bb9130748004a
michelg85 <miguelguyot85@gmail.com>
2022-09-07 14:09:31 -0300

    cambios en JS

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
-// Mi idea de proyecto final es poder aplicar javascript a la pagina que hice en el curso de desarrollo web y crear un carrito de compras.
-// La primer compra tiene un descuento del 40% off así que el cliente va a poder aplicar ese descuento en la compra final.
+// Mi idea de proyecto final es poder aplicar javascript a la pagina que hice en el curso de desarrollo web y crear una suscripción para descuentos mensuales y un carrito. 
+// Ingresando el cupon TAIKOBOUNTY obtenés un descuento del 40% en la suscripción. 
 
 //Simulador para calcular el monto total de tu compra
-function calcularPrecio (precioProducto, cantidadProducto, porcentajeDescuento, costoEnvio) {
-    //calculo los descuentos
-    let descuento = (precioProducto * porcentajeDescuento) / 100;
-    //se lo aplico al precio
-    let precioConDescuento = precioProducto - descuento;
-    //retorno el precio final con descuento y costo de envio, multiplicado por la cantidad de combos
-    return (precioConDescuento * cantidadProducto) + costoEnvio;
+function calcularPrecio (precioSuscripcion, mesesSuscripcion, cupon) {
+
+    console.log(cupon)
+    
+    //calculo los descuentos y los aplico si el cupon es válido
+    if (cupon == "TAIKOBOUNTY"){
+        precioSuscripcion = (precioSuscripcion * 0.6);
+    } else {
+        alert("El cupon no es válido, se cobrará el total de la suscripción...")
+    }
+
+    // calculo el total
+    let total = precioSuscripcion*mesesSuscripcion
+    
+    //retorno el precio final con descuento * la cantidad de meses
+    return total;
 }
 
-const envio = 800; //valor aproximado del costo de envio promedio dentro de CABA
 
-//le pedimos al usuario el precio de los productos, la cantidad y el porcentaje del descuento. 
-let producto = parseFloat(prompt("Ingrese el precio del producto que desea comprar:"));
-let cantidad = parseInt(prompt("Ingrese la cantidad de unidades que desea comprar de este producto:"));
-let direccion = parseInt(prompt("Ingrese su dirección (solo envíos dentro de CABA):"))
+const producto = 1500 
+//le pedimos al usuario que ingrese la cantidad de meses que quiere suscribirse, la dirección, mail y si tiene un cupón de descuento para ingresar.  
+let cantidadMeses = parseInt(prompt("Ingrese la cantidad de meses de suscripción:"));
+let direccion = parseInt(prompt("Ingrese su dirección a la que tenemos que enviar tu Taiko Bounty:"))
 let mail = parseInt(prompt("Ingresá tu mail para link de pago:"))
-let descuento = parseInt(prompt("Ingresá el número 40 para aplicar el descuento")) //los cupones en este caso serian los porcentajes de descuento, lo pongo asi para que quede mas visual
+let cupon = prompt("Si tenés un cupón de descuento, ingresalo aquí:") //los cupones en este caso serian los porcentajes de descuento, lo pongo asi para que quede mas visual
+
 
 //llamamos a la funcion y luego mostramos el precio final por alert
-let precioFinal = calcularPrecio(producto, cantidad, descuento, envio);
-alert("El precio total de tu compra es $" + precioFinal);
+let precioFinal = calcularPrecio(producto, cantidadMeses, cupon);
+alert("El precio total de tu suscripción es $" + precioFinal + ". Revisá tu correo ("+mail+") para finalizar el pago.");
 alert("¡Que lo disfrutes!");
